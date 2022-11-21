9de0c221f002e6bc6f4982e4548754a463c2b1de
Elias Cabeza <jose.cabeza@utec.edu.pe>
2022-11-20 19:39:06 -0500

    casi todo

--- control_cinematico/src/funciones.py
+++ control_cinematico/src/funciones.py
@@ -101,3 +101,84 @@ def ikine_kr4(xdes, q0):
         #    print("El algoritmo no llegó al valor deseado")
     
     return q, ee
+
+def jacobian_pose(q, delta=0.0001):
+    """
+    Jacobiano analitico para la posicion y orientacion (usando un
+    cuaternion). Retorna una matriz de 7x6 y toma como entrada el vector de
+    configuracion articular q=[q1, q2, q3, q4, q5, q6]
+
+    """
+    J = np.zeros((7,6))
+    # Implementar este Jacobiano aqui
+    T = fkine_kuka_kr4(q)
+    for i in range(6):
+        x = TF2xyzquat(T)
+        dq = copy(q)
+        dq[i] = dq[i] + delta
+        Tf = fkine_kuka_kr4(dq)
+        xf = TF2xyzquat(Tf)
+        J[0,i] = (xf[0] - x[0])/delta
+        J[1,i] = (xf[1] - x[1])/delta
+        J[2,i] = (xf[2] - x[2])/delta
+        J[3,i] = (xf[3] - x[3])/delta
+        J[4,i] = (xf[4] - x[4])/delta
+        J[5,i] = (xf[5] - x[5])/delta
+        J[6,i] = (xf[6] - x[6])/delta
+    
+    return J
+
+
+
+def rot2quat(R):
+    """
+    Convertir una matriz de rotacion en un cuaternion
+
+    Entrada:
+      R -- Matriz de rotacion
+    Salida:
+      Q -- Cuaternion [ew, ex, ey, ez]
+
+    """
+    dEpsilon = 1e-6
+    quat = 4*[0.,]
+
+    quat[0] = 0.5*np.sqrt(R[0,0]+R[1,1]+R[2,2]+1.0)
+    if ( np.fabs(R[0,0]-R[1,1]-R[2,2]+1.0) < dEpsilon ):
+        quat[1] = 0.0
+    else:
+        quat[1] = 0.5*np.sign(R[2,1]-R[1,2])*np.sqrt(R[0,0]-R[1,1]-R[2,2]+1.0)
+    if ( np.fabs(R[1,1]-R[2,2]-R[0,0]+1.0) < dEpsilon ):
+        quat[2] = 0.0
+    else:
+        quat[2] = 0.5*np.sign(R[0,2]-R[2,0])*np.sqrt(R[1,1]-R[2,2]-R[0,0]+1.0)
+    if ( np.fabs(R[2,2]-R[0,0]-R[1,1]+1.0) < dEpsilon ):
+        quat[3] = 0.0
+    else:
+        quat[3] = 0.5*np.sign(R[1,0]-R[0,1])*np.sqrt(R[2,2]-R[0,0]-R[1,1]+1.0)
+
+    return np.array(quat)
+
+
+def TF2xyzquat(T):
+    """
+    Convert a homogeneous transformation matrix into the a vector containing the
+    pose of the robot.
+
+    Input:
+      T -- A homogeneous transformation
+    Output:
+      X -- A pose vector in the format [x y z ew ex ey ez], donde la first part
+           is Cartesian coordinates and the last part is a quaternion
+    """
+    quat = rot2quat(T[0:3,0:3])
+    res = [T[0,3], T[1,3], T[2,3], quat[0], quat[1], quat[2], quat[3]]
+    return np.array(res)
+
+
+def skew(w):
+    R = np.zeros([3,3])
+    R[0,1] = -w[2]; R[0,2] = w[1]
+    R[1,0] = w[2];  R[1,2] = -w[0]
+    R[2,0] = -w[1]; R[2,1] = w[0]
+    return R
